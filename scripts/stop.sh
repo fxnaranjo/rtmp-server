94b50186888657799e2a38308f3a8044be9d9dcc
Francisco Naranjo <fxnaranjo@gmail.com>
2022-02-11 22:42:13 -0500

    debug

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -2,9 +2,9 @@
 
 echo Shut down streaming Container:$1
 
-docker stop $1
+#docker stop $1
 
-docker rm $1
+#docker rm $1
 
 echo "**************************************************************"
 
@@ -12,22 +12,31 @@ if [ ! -d /library/$2 ]
 
 then
 
-     mkdir -p /library/$2
+     #mkdir -p /library/$2
 
 fi
 
 
 
-crontab -l > /rtmp-server/scripts/mycron
+#crontab -l > /rtmp-server/scripts/mycron
 
-sed -i '/'$1'/d' /rtmp-server/scripts/mycron
+#sed -i '/'$1'/d' /rtmp-server/scripts/mycron
 
-crontab /rtmp-server/scripts/mycron
+#crontab /rtmp-server/scripts/mycron
 
-rm -fr /rtmp-server/scripts/mycron
+#rm -fr /rtmp-server/scripts/mycron
+
+theFile=$(ls)
+
+videoTime=$(date +"%H:%M")
+
+echo $theFile
+
+echo $videoTime
+
+#mv /videos/clubs/$2/$3/$4/$thefile  /videos/clubs/$2/$3/$4/$thefile
+
+#/library/$2
 
 
-mv /videos/clubs/$2/$3/$4/* /library/$2
-
-
-sed -i '/'$1'/d' /rtmp-server/scripts/active.log
+#sed -i '/'$1'/d' /rtmp-server/scripts/active.log
