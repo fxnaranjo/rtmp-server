63d4c83f92e94f9a8fb2f7579cfa4d623b4705ee
Francisco Naranjo <fxnaranjo@gmail.com>
2022-04-06 19:20:06 -0500

    id record

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -64,7 +64,7 @@ googleCloudStorage="https://storage.googleapis.com/"$clubname"/"$newVideo;
 
 endTime=$(date +"%m-%d-%Y %H:%M:%S");
 
-PGPASSWORD=acetv2022 psql -h 10.70.208.3 -A -t -U acetvdev -d sportpro -c "UPDATE stream.live set islive = false , videopath='"$googleCloudStorage"', endtime='\"$endTime\"' where STREAM.live.id ='"$5"'"
+PGPASSWORD=acetv2022 psql -h 10.70.208.3 -A -t -U acetvdev -d sportpro -c "UPDATE stream.live set islive = false , videopath='"$googleCloudStorage"', endtime='\"$endTime\"' where STREAM.live.id ='"$record"'"
 
 
 sed -i '/'$dockerName'/d' /rtmp-server/scripts/active.log
