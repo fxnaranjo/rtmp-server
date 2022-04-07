05dae9e1f9286c7fbc1808fdb6f6474589018f07
Francisco Naranjo <fxnaranjo@gmail.com>
2022-04-06 19:51:19 -0500

    clean

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -5,4 +5,10 @@ rm -fr /videos/clubs/*
 
 echo "" > /rtmp-server/scripts/active.log
 
+echo "" > /rtmp-server/scripts/aux
+
+crontab /rtmp-server/scripts/aux
+
+rm -fr /rtmp-server/scripts/aux
+
 echo "DONE CLEAN"
