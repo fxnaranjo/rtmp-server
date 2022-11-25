f6b89c1e13521fc249997d1ad18bc068cab9dde9
Francisco Naranjo <fxnaranjo@gmail.com>
2022-11-25 00:57:36 -0500

    fix parameters

--- scripts/createHL.sh
+++ scripts/createHL.sh
@@ -1,24 +1,20 @@
 clubname=$1
 highlight=$2
-camera=$3
-port=$4
-port2=$5
-tiempo=$6
-startHL=$7
-stopHL=$8
-user=$9
-private=$10
-description=$11
-videoPath=$12
+port=$3
+startHL=$4
+stopHL=$5
+user=$6
+private=$7
+description=$8
+videoPath=$9
 
+tiempo=1
 cd /library/$clubname
 
 echo "*****************************************************************************************" >> hl.log
 echo $clubname >> hl.log
 echo $highlight >> hl.log
-echo $camera >> hl.log
 echo $port >> hl.log
-echo $port2 >> hl.log
 echo $tiempo >> hl.log
 echo $startHL >> hl.log
 echo $stopHL >> hl.log
@@ -27,13 +23,21 @@ echo $private >> hl.log
 echo $description >> hl.log
 echo $videoPath >> hl.log
 
+
+extension1=".mp4"
+extension2=".jpg"
+
 videoTime=$(date +"%d%m%Y%H%M%S");
 
 streamId=$user-$clubname-$videoTime;
 
-newVideo="/library/"$clubname"/"$highlight
+newVideo="/library/"$clubname"/"$highlight$extension1
+newPhoto=$highlight$extension2
 
 
+echo "---------------------------------------------" >> hl.log
+echo "New Video:"$newVideo >> hl.log
+echo "New Photo:"$newPhoto >> hl.log
 
 #######################  DATABASE ACTIONS  ##########################
 idCamera=$(PGPASSWORD=F020kw31xx! psql -h 10.70.208.3 -A -t -U sportprodb -d sportpro -c 'SELECT c.id from stream.camera c where c.liveport='$port)
@@ -48,15 +52,12 @@ ffmpeg -i $videoPath -map 0 -ss $startHL -to $stopHL -c copy $newVideo
 echo "Video created" >> hl.log
 
 
-extension3=".jpg"
-
-newPhoto=$user-$videoTime$extension3
 
 ffmpeg -i $newVideo -r 1 -ss 00:00:01 -vf scale=320:180 -t 1 $newPhoto
 
 echo "Photo created" >> hl.log
 
-googleCloudStorage="https://storage.googleapis.com/"$clubname"/"$highlight;
+googleCloudStorage="https://storage.googleapis.com/"$clubname"/"$highlight$extension1;
 googleCloudStorage2="https://storage.googleapis.com/"$clubname"/"$newPhoto;
 
 
